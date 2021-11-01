ec40f7b4bb968a7cc18fcd39229720a8d8e20d46
Dany CM <danycmwow@gmail.com>
2021-11-01 11:35:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+""" from bruteforce import *
+
+
+print('Main Menu:')
+print('1. Bruteforce')
+print('2. Optimized')
+print('3. test')
+
+choice = int(input('Select an option: '))
+
+if choice == 1:
+    print('')
+     """
+
