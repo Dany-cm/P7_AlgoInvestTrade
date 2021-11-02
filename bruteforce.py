bab40cba75f2f760d4aff4215f05d83fc628fdf3
Dany CM <danycmwow@gmail.com>
2021-11-02 14:48:02 +0100

    Calculate the profit percentage and only show the 2 decimal digits

--- bruteforce.py
+++ bruteforce.py
@@ -1,28 +1,40 @@
 import csv
 
+
 def get_info_from_file(file):
-
-    action_list = []
-
-    with open(file, newline='') as file:
+    """Get information from a selected file"""
+    with open(file, newline="") as file:
         reader = csv.DictReader(file)
+        action_list = []
         for row in reader:
-            action_list.append((row['Actions'], float(row['price']), float(row['profit'])))
-            
+            action_list.append(
+                [
+                    row["name"],
+                    float(row["price"]),
+                    (float(row["price"]) * float(row["profit"]) / 100),
+                ]
+            )
         return action_list
 
-def bruteforce(capacite, actions_available, actions_selected = []):
 
+def bruteforce(capacite, actions_available, actions_selected=[]):
+    """Find all possible possibilities given the capacite"""
     if actions_available:
-        profit_1, action_profit_1 = bruteforce(capacite, actions_available[1:], actions_selected)
+        profit_1, action_profit_1 = bruteforce(
+            capacite, actions_available[1:], actions_selected
+        )
         currentvalue = actions_available[0]
 
         if currentvalue[1] <= capacite:
-            profit_2, action_profit_2 = bruteforce(capacite - currentvalue[1], actions_available[1:], actions_selected + [currentvalue])
+            profit_2, action_profit_2 = bruteforce(
+                capacite - currentvalue[1],
+                actions_available[1:],
+                actions_selected + [currentvalue],
+            )
 
             if profit_1 < profit_2:
                 return profit_2, action_profit_2
 
         return profit_1, action_profit_1
     else:
-        return sum([i[2] for i in actions_selected]), actions_selected
+        return round(sum([i[2] for i in actions_selected]), 2), actions_selected
