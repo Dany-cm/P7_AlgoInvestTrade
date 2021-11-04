9ec6f4bd78e7b598e024ec397f86c2b171cf97ba
Dany CM <danycmwow@gmail.com>
2021-11-04 19:39:56 +0100

    Use timeit and show the 5 numbers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bruteforce import bruteforce
 from optimized import optimized
 from file import get_info_from_file
-import time
+import timeit
 
 def Menu():
     print('Menu Principal:')
@@ -14,20 +14,22 @@ while True:
     try:
         choice = int(input('Select an option: '))
         if choice == 1:
-            start = time.time()
-            result = bruteforce(500, get_info_from_file(input('Select a CSV file: ')))
+            choice = input('Select a CSV file: ')
+            start = timeit.default_timer()
+            result = bruteforce(500, get_info_from_file(choice))
+            end_time = round(timeit.default_timer() - start, 2)
             print("Voici la liste d'action les plus rentable pour un budget maximum de 500€", '\n')
             print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in result[1]]), '\n')
             print(f"Notre bénéfice total s'élève à {result[0]} €", '\n')
-            end_time = round(time.time() - start, 2)
             print(f"Temps d'exécution: {end_time} secondes", '\n')
         if choice == 2:
-            start = time.time()
-            result = optimized(500, get_info_from_file(input('Select a CSV file: ')))
+            choice = input('Select a CSV file: ')
+            start = timeit.default_timer()
+            result = optimized(500, get_info_from_file(choice))
+            end_time = round(timeit.default_timer() - start, 5)
             print("Voici la liste d'action les plus rentable pour un budget maximum de 500€", '\n')
             print('\n'.join([str(x) for x in result]), '\n')
-            print(f'Notre bénéfice total s"élève à {round(sum([x[2] for x in result]), 2)}€', '\n')
-            end_time = round(time.time() - start, 2)
+            print(f"Notre bénéfice total s'élève à {round(sum([x[2] for x in result]), 2)}€", '\n')
             print(f"Temps d'exécution: {end_time} secondes", '\n')
         if choice == 3:
             quit()
