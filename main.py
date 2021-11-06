77e9f86fc7fffd956b34467160941740d803c229
Dany CM <danycmwow@gmail.com>
2021-11-06 18:02:32 +0100

    Fix algo naive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bruteforce import bruteforce
-from optimized import optimized
+from optimized_naive import optimized_naive
 from optimized_dynamic import optimized_dynamic
 
 from file import get_info_from_file
@@ -8,45 +8,61 @@ import timeit
 def Menu():
     print('Menu Principal:')
     print('1. Force brute')
-    print('2. Optimisé')
+    print('2. Optimisé Naïve')
     print('3. Optimisé Dynamique')
     print('4. Quitter')
 
 while True:
     Menu()
+    print()
     try:
         choice = int(input('Sélectionner une option: '))
         if choice == 1:
             choice = input('Sélectionner un fichier CSV: ')
+            print()
             start = timeit.default_timer()
             result = bruteforce(500, get_info_from_file(choice))
             end_time = round(timeit.default_timer() - start, 2)
-            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€", '\n')
+            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€")
+            print()
             for action in result[1]:
                 print(action)
-
-            print(f"Notre bénéfice total s'élève à {result[0]}€", '\n')
-            print(f"Temps d'exécution: {end_time} secondes", '\n')
+            print()
+            print(f"Notre bénéfice total s'élève à {result[0]}€")
+            print()
+            print(f"Temps d'exécution: {end_time} secondes")
+            print()
         if choice == 2:
             choice = input('Sélectionner un fichier CSV: ')
+            print()
             start = timeit.default_timer()
-            result = optimized(500, get_info_from_file(choice))
-            end_time = round(timeit.default_timer() - start, 2)
-            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€", '\n')
-            for action in result[1]:
+            result = optimized_naive(500, get_info_from_file(choice))
+            end_time = round(timeit.default_timer() - start, 4)
+            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€")
+            print()
+            for action in result:
                 print(action)
-            print(f"Notre bénéfice total s'élève à {result[0]}€", '\n')
-            print(f"Temps d'exécution: {end_time} secondes", '\n')
+            benefice = sum(map(lambda action: action.benefice, result))
+            print()
+            print(f"Notre bénéfice total s'élève à {benefice:>.2f}€")
+            print()
+            print(f"Temps d'exécution: {end_time} secondes")
+            print()
         if choice == 3:
             choice = input('Sélectionner un fichier CSV: ')
+            print()
             start = timeit.default_timer()
             result = optimized_dynamic(500, get_info_from_file(choice))
             end_time = round(timeit.default_timer() - start, 2)
-            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€", '\n')
+            print(f"Voici la liste d'action les plus rentable pour un budget maximum de 500€")
+            print()
             for action in result[1]:
                 print(action)
-            print(f"Notre bénéfice total s'élève à {result[0]}€", '\n')
-            print(f"Temps d'exécution: {end_time} secondes", '\n')
+            print()
+            print(f"Notre bénéfice total s'élève à {result[0]:>.2f}€")
+            print()
+            print(f"Temps d'exécution: {end_time} secondes")
+            print()
         if choice == 4:
             quit()
     except ValueError:
--- optimized_naive.py
+++ optimized_naive.py
@@ -1,4 +1,4 @@
-def optimized(capacite, actions):
+def optimized_naive(capacity, actions):
     """ Sort by profit in descending order """
 
     best_action = []
@@ -6,7 +6,7 @@ def optimized(capacite, actions):
     sorted(actions, key=lambda x: x.profit, reverse=True)
 
     for action in actions:
-        if action.price <= capacite:
-            capacite = capacite - action.price
+        if action.price <= capacity:
+            capacity = capacity - action.price
             best_action.append(action)
     return best_action
